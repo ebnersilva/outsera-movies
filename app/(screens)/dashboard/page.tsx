5a128c2a7e359ce440189b545b4be698a336a8d4
ebner <ebner.suporte@hotmail.com>
2024-10-22 20:33:43 -0300

    refactor: prettier configs and identation

--- app/(screens)/dashboard/page.tsx
+++ app/(screens)/dashboard/page.tsx
@@ -1,28 +1,27 @@
-'use client'
-import { ListYearMultipleWinners } from './components/ListYearMultipleWinners';
-import { TopThreeStudiosWithWinners } from './components/TopThreeStudiosWithWinners';
-import { MaxMinIntervalProducers } from './components/MaxMinIntervalProducers';
-import { ListMovieWinnerByYear } from './components/ListMovieWinnerByYear';
+'use client';
+import {ListYearMultipleWinners} from './components/ListYearMultipleWinners';
+import {TopThreeStudiosWithWinners} from './components/TopThreeStudiosWithWinners';
+import {MaxMinIntervalProducers} from './components/MaxMinIntervalProducers';
+import {ListMovieWinnerByYear} from './components/ListMovieWinnerByYear';
 
 export default function Dashboard() {
+	return (
+		<div className="flex flex-1 flex-col p-20 gap-10 bg-slate-700">
+			<h1>Dashboard</h1>
 
-  return (
-    <div className="flex flex-1 flex-col p-20 gap-10 bg-slate-700">
-      <h1>Dashboard</h1>
+			{/* First Line */}
+			<div className="flex flex-row sm:flex-col md:flex-col lg:flex-row rounded-md gap-10">
+				<ListYearMultipleWinners />
 
-      {/* First Line */}
-      <div className="flex flex-row sm:flex-col md:flex-col lg:flex-row rounded-md gap-10">
-        <ListYearMultipleWinners />
+				<TopThreeStudiosWithWinners />
+			</div>
 
-        <TopThreeStudiosWithWinners />
-      </div>
+			{/* Second Line */}
+			<div className="flex flex-row gap-10 flex-wrap">
+				<MaxMinIntervalProducers />
 
-      {/* Second Line */}
-      <div className="flex flex-row gap-10 flex-wrap">
-          <MaxMinIntervalProducers />
-
-          <ListMovieWinnerByYear />
-      </div>
-    </div>
-  );
+				<ListMovieWinnerByYear />
+			</div>
+		</div>
+	);
 }
