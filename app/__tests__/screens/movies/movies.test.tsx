1e788b431d147b86d8586c8b1437c6bbd253db94
ebner <ebner.suporte@hotmail.com>
2024-10-22 15:46:43 -0300

    test: improve test directories

--- app/__tests__/screens/movies/movies.test.tsx
+++ app/__tests__/screens/movies/movies.test.tsx
@@ -1,6 +1,6 @@
 import { it, describe } from 'vitest'
 import { render } from '@testing-library/react'
-import Movies from '../../../movies/page'
+import Movies from '../../../(screens)/movies/page'
  
 describe('Movies', () => {
     it('should render Movies screen' , () => {
