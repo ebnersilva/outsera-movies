2773e68669979e35d814175d5777a00f71ae850f
ebner <ebner.suporte@hotmail.com>
2024-10-22 00:39:16 -0300

    test: improve test folders structure

--- app/__tests__/screens/dashboard/dashboard.test.tsx
+++ app/__tests__/screens/dashboard/dashboard.test.tsx
@@ -1,6 +1,6 @@
 import { it, describe, expect } from 'vitest'
 import { render } from '@testing-library/react'
-import Dashboard from '../../dashboard/page'
+import Dashboard from '../../../dashboard/page'
  
 describe('Dashboard', () => {
     it('should render Dashboard component' , () => {
--- app/__tests__/screens/movies/movies.test.tsx
+++ app/__tests__/screens/movies/movies.test.tsx
@@ -1,6 +1,6 @@
 import { it, describe } from 'vitest'
 import { render } from '@testing-library/react'
-import Movies from '../../movies/page'
+import Movies from '../../../movies/page'
  
 describe('Movies', () => {
     it('should render Movies screen' , () => {
