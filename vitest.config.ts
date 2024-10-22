7c2874f85ad58c396b4c6034785bd9e779c81ea5
ebner <ebner.suporte@hotmail.com>
2024-10-22 15:46:21 -0300

    test: add relative path to vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,13 @@
 import { configDefaults, defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
+import path from 'path'
  
 export default defineConfig({
   plugins: [react()],
   test: {
+    alias: {
+      '~': path.resolve(__dirname, "./")
+    },
     exclude: [...configDefaults.exclude, 'next.config.mjs'],
     environment: 'jsdom',
     setupFiles: ['./vitest.setup.js'],
