2b0b4b37003b051cedf9b2fe6eac42cb5186de00
ebner <ebner.suporte@hotmail.com>
2024-10-22 17:01:16 -0300

    feat: add input search year

--- app/(screens)/dashboard/components/ListMovieWinnerByYear.tsx
+++ app/(screens)/dashboard/components/ListMovieWinnerByYear.tsx
@@ -1,10 +1,12 @@
 import { MagnifyingGlassIcon } from "@radix-ui/react-icons"
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Table } from "~/app/components/Table"
 import { useAppStore } from "~/app/store";
 import { WinnerByYear } from "~/app/store/winnersByYear/types";
+import { showErrorToast } from "~/app/utils/showToast";
 
 export const ListMovieWinnerByYear = () => {
+    const [filteredYear, setFilteredYear] = useState('');
 
     const {
         winnersByYear,
@@ -17,13 +19,33 @@ export const ListMovieWinnerByYear = () => {
     fetchWinnersByYearApi();
     }, []);
 
+    const handleOnSearch = () => {
+        if (!filteredYear) {
+            showErrorToast('Informe um ano válido')
+            return;
+        }
+        
+        fetchWinnersByYearApi(parseInt(filteredYear))
+    }
+
     return (
         <div className="flex flex-col flex-1 bg-slate-500 px-4 py-4 rounded-md gap-4">
             <strong>List movie winners by year</strong>
 
             <div className="flex flex-row w-full gap-2">
-            <input className="flex w-full rounded-sm h-8 p-2 border-none text-slate-900" type="number" placeholder="Search by year" />
-            <button className="flex items-center justify-center bg-slate-900 rounded-sm h-8 w-8" type="button">
+
+            <input 
+                className="flex w-full rounded-sm h-8 p-2 border-none text-slate-900" 
+                type="number" 
+                placeholder="Search by year" 
+                onChange={e => setFilteredYear(e.target.value)} 
+            />
+
+            <button 
+                className="flex items-center justify-center bg-slate-900 rounded-sm h-8 w-8" 
+                type="button"
+                onClick={handleOnSearch}
+            >
                 <MagnifyingGlassIcon />
             </button>
             </div>
