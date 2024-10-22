253fb6de56a1d3aa83dda24edc317510d92307da
ebner <ebner.suporte@hotmail.com>
2024-10-22 00:37:33 -0300

    test: add a test to global Table component

---
+++ app/__tests__/components/Table.test.tsx
@@ -0,0 +1,72 @@
+import { it, describe, expect } from 'vitest'
+import { render, screen } from '@testing-library/react'
+import {IColumn, Table} from '../../components/Table'
+
+interface IFirstTableTest {
+    id: number;
+    title: string;
+    value: number
+}
+ 
+describe('Table', () => {
+    it('should render Table' , () => {        
+        const wrapper = render(<Table columns={[]} data={[]}  />)
+
+        expect(wrapper).toBeTruthy();
+    })
+
+    it('should render Table with correct values' , () => {
+        const columns: IColumn<IFirstTableTest>[] = [
+            {
+                id: 1,
+                title: 'Id',
+                property: 'id'
+            },
+            {
+                id: 2,
+                title: 'Title',
+                property: 'title'
+            },
+            {
+                id: 3,
+                title: 'Value',
+                property: 'value'
+            }
+        ]
+
+        const data: IFirstTableTest[] = [
+            {
+                id: 1000,
+                title: 'Title 1',
+                value: 1
+            },
+            {
+                id: 1001,
+                title: 'Title 2',
+                value: 2
+            },
+            {
+                id: 1002,
+                title: 'Title 3',
+                value: 3
+            }
+        ]
+
+        const wrapper = render(<Table columns={columns} data={data}  />)
+
+        wrapper.debug()
+        
+        // Verificar os cabeçalhos das colunas
+        columns.forEach((column) => {
+            expect(screen.getByText(column.title)).toBeInTheDocument();
+        });
+    
+        // Verificar o conteúdo das linhas da tabela
+        data.forEach((row) => {
+            expect(screen.getByText(row.id.toString())).toBeInTheDocument();
+            expect(screen.getByText(row.title)).toBeInTheDocument();
+            expect(screen.getByText(row.value.toString())).toBeInTheDocument();
+        });
+
+    })
+})
