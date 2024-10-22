a89c04134d960e60efe7d2c9e84f0cc134a39898
ebner <ebner.suporte@hotmail.com>
2024-10-22 13:55:11 -0300

    refactor: improve vitest configs

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vitest/config'
+import { configDefaults, defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
  
 export default defineConfig({
   plugins: [react()],
   test: {
+    exclude: [...configDefaults.exclude, 'next.config.mjs'],
     environment: 'jsdom',
-    setupFiles: ['./vitest.setup.js']
+    setupFiles: ['./vitest.setup.js'],
   },
 })
